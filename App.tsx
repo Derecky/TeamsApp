41038c6d08d96f9ca81e376601bc12b480972456
Derecky Costa <dereckycosta@gmail.com>
2022-09-18 16:01:31 -0300

    chore: initial commit with styled-components

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Teams App!</Text>
+      <StatusBar style="auto" translucent/>
+    </View>
+  );
+}
